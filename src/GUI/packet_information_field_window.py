b3e2b3dd8c2fbc3047d86869666dc423bb5108ef
revil0mg <oliver_mtz_g@yahoo.com>
2018-03-22 01:15:12 -0600

    Fixed Bug, repeating entries
    If you type on the added entries using the + button, then all the text fields that were added would have that text too.

--- src/GUI/packet_information_field_window.py
+++ src/GUI/packet_information_field_window.py
@@ -12,8 +12,8 @@ import ttk
 
 class PacketInformationFieldWindow(tk.Frame):
     def _new_entry(self, frame, c, r):
-        tk.Entry(frame, text = " ").grid(column = c, row = r+1)
-        tk.Entry(frame, text = " ").grid(column = c+1, row = r+1)
+        tk.Entry(frame, text = "").grid(column = c, row = r+1)
+        tk.Entry(frame, text = "").grid(column = c+1, row = r+1)
         self.column = 0
         self.row +=1
     def init_window(self):
