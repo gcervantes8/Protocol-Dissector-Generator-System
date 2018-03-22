9d33209b92b5239f993cb21c69b3943c0809bd1c
revil0mg <oliver_mtz_g@yahoo.com>
2018-03-21 23:48:27 -0600

    Added project navigator window

---
+++ src/GUI/project_navigator_window.py
@@ -0,0 +1,57 @@
+#! python2
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  2 13:29:57 2018
+@author: Gerardo Cervantes
+         Oliver Martinez
+         Isaac Hoffman
+         Daniel Ornelas
+         Christopher Soto
+"""
+
+#TODO: change workspace_label to current workspace
+
+import Tkinter as tk
+
+
+class ImportProject(tk.Frame):
+    def _create_widgets(self):
+
+        desc = tk.Label(self.root, text="Workspace X", pady=10)
+        desc.pack()
+
+        frame1 = tk.Frame(self.root)
+        frame1.pack(side=tk.LEFT)
+
+        project_button_1 = tk.Button(frame1, command = self.continue_button_clicked, text="Project A", padx=10)
+        project_button_2 = tk.Button(frame1, command = self.continue_button_clicked, text="Project B", padx=10)
+        project_button_3 = tk.Button(frame1, command = self.continue_button_clicked, text="Project C", padx=10)
+
+        project_button_1.grid(row=0, column=0, padx=10)
+        project_button_2.grid(row=1, column=0, padx=10)
+        project_button_3.grid(row=2, column=0, padx=10)
+
+    def continue_button_clicked(self):
+        #Insert Opening project functionality
+        print('Continue button click')
+
+
+    def __init__(self, parent):
+        tk.Frame.__init__(self, parent)
+        self.root = parent
+        self.root.title("Import Project")
+
+        # block to control where window opens
+        w = self.root.winfo_screenwidth()
+        h = self.root.winfo_screenheight()
+        w = w/2
+        h = h/2
+        self.root.geometry("150x150+%d+%d" % (w-75, h-200))
+
+        self._create_widgets()
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = ImportProject(parent=root)
+    app.mainloop()
--- src/GUI/reference_list_field.py
+++ src/GUI/reference_list_field.py
@@ -6,7 +6,7 @@ Created on Wed Mar 21 22:05:42 2018
 """
 
 #TODO: Add Reference List Name field on top of window. (see SRS for details)
-#TODO: Make + button work properly
+#TODO: Make + button work properly ??
 
 import Tkinter as tk
 import ttk
@@ -15,7 +15,8 @@ class Reference_List_Window(tk.Frame):
     def _new_entry(self, frame, c, r):
         tk.Entry(frame, text = " ").grid(column = c, row = r+1)
         tk.Entry(frame, text = " ").grid(column = c+1, row = r+1)
-
+        self.column = 0
+        self.row +=1
     def init_window(self):
 
         #root.wm_title('Packet Information')
@@ -28,7 +29,7 @@ class Reference_List_Window(tk.Frame):
         entry1.grid(column = 0 , row = 1)
         entry2 = ttk.Entry(frame)
         entry2.grid(column = 1 , row = 1)
-        tk.Button(frame, text="+",command = lambda: self._new_entry(frame,0,1)).grid(column = 3, row = 2)
+        tk.Button(frame, text="+",command = lambda: self._new_entry(frame,self.column,self.row)).grid(column = 3, row = 2)
 
 
 
@@ -36,6 +37,8 @@ class Reference_List_Window(tk.Frame):
         tk.Frame.__init__(self,parent)
         self.root = parent
         self.root.title("Refernce List [Reference List Name]")
+        self.column = 0
+        self.row = 1
         self.init_window()
 
 if __name__ == "__main__":
