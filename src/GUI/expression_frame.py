a626bc60909b630ae43cea81dfde77cbbebb1e7a
Jerry C <gcervantes8@miners.utep.edu>
2018-04-27 11:33:00 -0600

    Cleaned up the code in expression

--- src/GUI/expression_frame.py
+++ src/GUI/expression_frame.py
@@ -15,23 +15,12 @@ class Expression_frame(tk.Label):
     def __init__(self, master=None):
         self.root = master
         path = '../Images/Expression.gif'
-        ph = tk.PhotoImage(file = path)
+        image = tk.PhotoImage(file = path)
 
-        tk.Label.__init__(self, master, image = ph)
-        self.image = ph
-#        self.pack()
-#        self._create_widgets()
-#        
-#        
-#
-#    def _create_widgets(self):
-#        
-#        main_window = tk.Frame(self)
-#        main_window.pack(side="top")
+        tk.Label.__init__(self, master, image = image)
+        self.image = image
         
 if __name__ == "__main__":
-#    root = tk.Tk()
     root = tk.Toplevel()
-    
     app = Expression_frame(master=root)
     app.mainloop()
