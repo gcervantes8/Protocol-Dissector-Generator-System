f6b0373fd438da3765f6b5879cb8831f2fcdacf2
ihhoffmann <ihhoffmann@miners.utep.edu>
2018-05-06 21:50:21 -0600

    Create Workspace.py
    what i have so far still working

---
+++ src/Model/Workspace.py
@@ -0,0 +1,29 @@
+import os
+from Project import Project
+
+class Workspace(object):
+    path = str()
+    name = str()
+
+    # Getter Methods
+    def getPath(self):
+        return self.path
+
+    def getName(self):
+        return self.name
+
+    def get_projects_lists(self):
+        a = []
+        for root, dirs, files in os.walk(self.path):
+            for file in files:
+                if file.endswith(".txt"): # .txt just to test change to .xml later
+                    a.append(os.path.join(root, file))
+        return a
+
+
+    # Setter Methods
+    def setPath(self, path):
+        self.path = path
+
+    def setName(self, name):
+        self.name = name
