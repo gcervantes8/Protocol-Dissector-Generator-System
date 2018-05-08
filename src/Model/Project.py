2c7958c3686073c87aa39938848771120e5a412f
ihhoffmann <ihhoffmann@miners.utep.edu>
2018-05-08 17:38:11 -0600

    can create xml still working on it

--- src/Model/Project.py
+++ src/Model/Project.py
@@ -1,3 +1,35 @@
+import os
+import xml.etree.ElementTree as et
+from Workspace import Workspace
+
 
 class Project(object):
-    print "fill in later"
+        name = str()
+        desc = str()
+
+        def getPath(self):
+            return self.path
+
+        def getDesc(self):
+            return self.desc
+
+        def setDesc(self, desc):
+            self.path = desc
+
+        def setName(self, name):
+            self.name = name
+
+        def loadProject(xml):
+            if xml is None:
+                return
+            else:
+                return Project('', '')
+
+        def createProject(self):
+            name = self.name
+            dirPath = Workspace.current.getPath()
+            xml = "<myxmldata/>"
+            f = open(dirPath +"/"+name+".xml", "wb")
+            f.write(xml)
+            f.close()
+
