dfe37f92d03c1ee659898c59289b902a5a3a8c29
Jerry C <gcervantes8@miners.utep.edu>
2018-05-14 13:26:53 -0600

    Added add button to project navigator

--- src/GUI/project_navigator_window.py
+++ src/GUI/project_navigator_window.py
@@ -22,18 +22,22 @@ from ProjectManager import ProjectManager
 class ProjectNavigatorWindow(MovingFrame):
     def _create_widgets(self,parent):
 
-        # desc = tk.Label(self.root, text="Workspace X", pady=10)
-        # desc.pack()
 
-        frame1 = tk.Frame(parent)
-        frame1.pack(side=tk.LEFT)
+        self.frame1 = tk.Frame(parent)
+        self.frame1.pack(side=tk.LEFT)
         a = ProjectManager.current.guiInfo(None, None, None, None, None, 2)
 
+        self.button_amount = 0
         self.ProjectButtons = []
         for i in range(len(a)):
-            self.ProjectButtons.append(tk.Button(frame1, command = self.continue_button_clicked, text=a[i], padx=10))
-            self.ProjectButtons[i].grid(row=i+1, column=0, padx=10)
+            self.add_button(a[i])
 
+    def add_button(self, button_name):
+        button = tk.Button(self.frame1, command = self.continue_button_clicked, text = button_name, padx = 10)
+        self.ProjectButtons.append(button)
+        self.ProjectButtons[self.button_amount].grid(row=self.button_amount+1, column=0, padx=10)
+        self.button_amount += 1
+        return button
     def continue_button_clicked(self):
         #Insert Opening project functionality
         print('Continue button click')
@@ -42,12 +46,6 @@ class ProjectNavigatorWindow(MovingFrame):
     def __init__(self, parent):
         self.root = parent
         self.mv = MovingFrame(self.root, "Project Navigator", 0, 100, 150, 200)
-        # block to control where window opens
-        # w = self.root.winfo_screenwidth()
-        # h = self.root.winfo_screenheight()
-        # w = w/2
-        # h = h/2
-        # self.root.geometry("150x150+%d+%d" % (w-75, h-200))
 
         self._create_widgets(self.mv.f)
 
