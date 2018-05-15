8242dfab3e44ca7e66d374c6c874bbba6a214014
Daniel Ornelas <dornelas4@miners.utep.edu>
2018-05-14 22:26:41 -0600

    added min max

--- src/GUI/moving_frame.py
+++ src/GUI/moving_frame.py
@@ -20,6 +20,10 @@ class MovingFrame:
         self.y = clamp(0, 1480-200, self.y + dy) # actual size here
         self.f.place_configure(x=self.x, y=self.y)
 
+    def minimize_maximize(self):
+        self.root.iconify()
+    def close_window(self):
+        self.destroy()
     def __init__(self, root, title, x, y,w,h):
         self.root = root
 
@@ -29,8 +33,15 @@ class MovingFrame:
 
 
         self.l = Label(self.f, bd=1, bg="#08246b", fg="white",text=title)
+
+
         self.l.pack(fill=X)
 
+        self.min_max_btn = Button(self.l,text='-',command=self.minimize_maximize)
+
+        self.close_btn = Button(self.l,text='X',command=self.close_window)
+        self.close_btn.pack(side=RIGHT)
+        self.min_max_btn.pack(side=RIGHT)
         self.l.bind('<1>', self.MoveWindowStart)
         self.f.bind( '<1>', self.focus)
         self.l.bind( '<B1-Motion>', self.MoveWindow)
@@ -49,6 +60,6 @@ if __name__ == "__main__":
     root = Tk()
     root.title("...")
     root.geometry("%dx%d%+d%+d"%(640, 480, 0, 0)) #useful for main window size
-    x = MovingFrame(root, "Window 1", 10, 10)
+    x = MovingFrame(root, "Window 1", 10, 10,100,100)
 
     root.mainloop()
